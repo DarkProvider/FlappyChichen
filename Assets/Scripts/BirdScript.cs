cb1e60256234ebddc9d191ac0d08e21f34b8e439
Estel <53404124+DarkProvider@users.noreply.github.com>
2021-06-30 22:42:44 +0200

    Initial Base Push

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BirdScript : MonoBehaviour
+{
+    public Vector3 flappyForce = new Vector3(0, 50, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            GetComponent<Rigidbody>().AddForce(flappyForce);
+        }
+
+        if (Input.GetKeyDown("space") && Time.timeScale == 0)
+        {
+
+            //Application.LoadLevel(Application.loadedLevel);
+            SceneManager.LoadScene(0);
+        }
+        {
+
+
+        }
+
+
+    }
+
+    void OnCollisionEnter()
+    {
+
+
+        Time.timeScale = 0;
+
+    }
+}
---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+using System.Drawing;
+
+public class GameHandler : MonoBehaviour
+{
+    public Vector3 velocity = new Vector3(-5, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameObject gameObject = new GameObject("Pipe", typeof(SpriteRenderer));
+        //gameObject.GetComponent<SpriteRenderer>.sprite =  ;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(velocity * Time.deltaTime, Space.World);
+
+        if (transform.position.x < -17)
+        {
+
+            transform.Translate(250, Random.Range(-2, 4), 0);
+
+        }
+    }
+}
